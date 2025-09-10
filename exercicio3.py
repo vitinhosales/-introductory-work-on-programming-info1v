291e5c7578ec074e7be8dbc3ad417f6baefc7dd3
vitinhosales <vitor.sales@escolar.ifrn.edu.br>
2025-09-10 17:55:21 -0300

    Create exercicio3.py
    adição de um novo arquivo python

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+n1 = int(input("digite sua primeira nota:"))
+n2 = int(input("digite sua segunda nota:"))
+n3 = int(input("digite sua terceira nota:"))
+n4 = int(input("digite sua quarta nota:"))
+
+soma = n1 + n2 + n3 + n4
+print(soma/4)
