32701be8852133ed7440ec4d9cb3fea5aefbea2d
vitinhosales <vitor.sales@escolar.ifrn.edu.br>
2025-09-17 18:01:22 -0300

    Update exercicioV24.py
    negocio de avista, pagamentos e tals

--- code insights - terceiro bimestre/exercicioV24.py
+++ code insights - terceiro bimestre/exercicioV24.py
@@ -4,6 +4,15 @@
 
 
 #begin_inputs
-valor_compra = 
+valor_compra = int(input("Digite o valor da sua compra:"))
 
 #end_inputs
+
+avista = int(valor_compra - valor_compra * 9/100)
+print(avista)
+
+prestacao_5x = int(valor_compra/5)
+print(prestacao_5x)
+
+prestacao_10x = int((valor_compra + valor_compra * 17/100)/10)
+print(prestacao_10x)
