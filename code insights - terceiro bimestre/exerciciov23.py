4edc910fd60b46ae1ba6fbf544d2aa9d1d39ab65
vitinhosales <vitor.sales@escolar.ifrn.edu.br>
2025-09-17 17:40:51 -0300

    Create exerciciov23.py
    exercicio v2.3 = media de 3 valores

---
+++ code insights - terceiro bimestre/exerciciov23.py
@@ -0,0 +1,13 @@
+# @cikey 8ac9e6fe4b013542d94d7c7ce2899e6c
+# @sid 20251174010072
+# @aid V2.3
+
+
+#begin_inputs
+a = int(input("digite o primeiro valor:"))
+b = int(input("digite o segundo valor:"))
+c = int(input("digite o terceiro valor:"))
+
+#end_inputs
+
+print("{}".format((a+b+c)/3))
