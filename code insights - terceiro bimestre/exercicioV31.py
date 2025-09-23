85f248662e7a1f5a70085f25fd0863c4a4b7b02b
vitinhosales <vitor.sales@escolar.ifrn.edu.br>
2025-09-23 14:08:08 -0300

    novos exercicios
    exercicios 3.1 e 3.2 do code insights

---
+++ code insights - terceiro bimestre/exercicioV31.py
@@ -0,0 +1,14 @@
+# @cikey 8ac9e6fe4b013542d94d7c7ce2899e6c
+# @sid 20251174010072
+# @aid V3.1
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99,251):
+    print(i, "bugs no software, pegue um deles e conserte...")
---
+++ code insights - terceiro bimestre/exercicioV32.py
@@ -0,0 +1,16 @@
+# @cikey 8ac9e6fe4b013542d94d7c7ce2899e6c
+# @sid 20251174010072
+# @aid V3.2
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99,251):
+    print(i , "bugs no software, pegue um deles e conserte...")
+    print("Tente Ctrl+F5")
+print("Vamos fazer mais um cafe!")
